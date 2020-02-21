0f2da70ba9eced37a494680ade2600896152a6ad
durcanm <mdurcan@gmail.com>
2020-02-21 21:24:54 +0300

    added xml output

--- CarBrandModel/Cars.Api/Startup.cs
+++ CarBrandModel/Cars.Api/Startup.cs
@@ -11,7 +11,8 @@ namespace Cars.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                    .AddXmlDataContractSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
