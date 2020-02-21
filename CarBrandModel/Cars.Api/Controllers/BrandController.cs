3fb00078b3f6c3ee35edf3f8cfcfae5c0ed88c0d
durcanm <mdurcan@gmail.com>
2020-02-21 20:15:04 +0300

    brand controller and model added

---
+++ CarBrandModel/Cars.Api/Controllers/BrandController.cs
@@ -0,0 +1,27 @@
+﻿using Cars.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Cars.Api.Controllers
+{
+    [ApiController]
+    [Route("api/brand")]
+    public class BrandController : ControllerBase
+    {
+        public BrandController()
+        { }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<BrandDto>> GetAllBrands()
+        {
+            var brandList = new List<BrandDto>()
+            {
+                new BrandDto { Id = 1, Name = "Ford" },
+                new BrandDto { Id = 2, Name = "Subaru" },
+                new BrandDto { Id = 3, Name = "Opel" },
+            };
+
+            return Ok(brandList);
+        }
+    }
+}
---
+++ CarBrandModel/Cars.Api/Models/BrandDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Cars.Api.Models
+{
+    public class BrandDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
